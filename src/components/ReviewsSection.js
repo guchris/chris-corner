ddcd8da042b3475c1f07c83ab11fd8f93955f4f2
Christopher Gu <cleegu@gmail.com>
2024-02-02 21:00:39 -0800

    Added to and updated reviews-data.json

--- src/components/ReviewsSection.js
+++ src/components/ReviewsSection.js
@@ -6,6 +6,7 @@ const ReviewsSection = ({ reviews }) => {
     const restaurantReviews = reviews.filter(review => review.type === "Restaurant");
     const cafeReviews = reviews.filter(review => review.type === "Cafe");
     const barReviews = reviews.filter(review => review.type === "Bar");
+    const dessertReviews = reviews.filter(review => review.type === "Dessert");
 
     return (
         <>
@@ -33,6 +34,16 @@ const ReviewsSection = ({ reviews }) => {
                 <h4 className="section-title">Seattle Bars</h4>
                 <div className="section-skinny">
                     {barReviews.map((review, index) => (
+                        <div key={review.id} className="row-skinny">
+                            <Link key={review.id} href={`/food/reviews/${review.slug}`} className="body-upper">{review.name}</Link>
+                        </div>
+                    ))}
+                </div>
+            </div>
+            <div className="section">
+                <h4 className="section-title">Seattle Desserts</h4>
+                <div className="section-skinny">
+                    {dessertReviews.map((review, index) => (
                         <div key={review.id} className="row-skinny">
                             <Link key={review.id} href={`/food/reviews/${review.slug}`} className="body-upper">{review.name}</Link>
                         </div>
